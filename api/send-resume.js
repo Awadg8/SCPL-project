24720ef3f9ec539024ec59e85e4ef140cfafcf79
awadg8 <awadheshg798@gmail.com>
2024-10-07 12:01:20 +0530

    Some changes done

--- api/send-resume.js
+++ api/send-resume.js
@@ -16,41 +16,52 @@ export default async function handler(req, res) {
   }
 
   if (req.method === "POST") {
-    const { name, email, phoneNumber, subject, message } = req.body;
-    const resumeFile = req.file;
+    // Handle file upload and other form fields
+    upload.single("resumeFile")(req, res, async (err) => {
+      if (err) {
+        return res
+          .status(400)
+          .json({ error: "File upload error", details: err });
+      }
 
-    if (!resumeFile) {
-      return res.status(400).send("No file uploaded.");
-    }
+      const { name, email, phoneNumber, subject, message } = req.body;
+      const resumeFile = req.file;
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.REACT_APP_GMAIL_USERNAME,
-        pass: process.env.REACT_APP_GMAIL_PASSWORD,
-      },
-    });
+      if (!resumeFile) {
+        return res.status(400).send("No file uploaded.");
+      }
 
-    const mailOptions = {
-      from: process.env.REACT_APP_GMAIL_USERNAME,
-      to: "awadheshproactii@gmail.com",
-      subject: "Resume Upload",
-      text: `Name: ${name}\nEmail: ${email}\nPhone Number: ${phoneNumber}\nMessage: ${message}`,
-      attachments: [
-        {
-          filename: resumeFile.originalname,
-          path: resumeFile.path,
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.REACT_APP_GMAIL_USERNAME,
+          pass: process.env.REACT_APP_GMAIL_PASSWORD,
         },
-      ],
-    };
+      });
 
-    try {
-      const info = await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: "Resume sent successfully", info });
-    } catch (error) {
-      console.error("Error sending resume:", error);
-      res.status(500).json({ error: "Failed to send resume", details: error });
-    }
+      const mailOptions = {
+        from: process.env.REACT_APP_GMAIL_USERNAME,
+        to: "awadheshproactii@gmail.com",
+        subject: "Resume Upload",
+        text: `Name: ${name}\nEmail: ${email}\nPhone Number: ${phoneNumber}\nMessage: ${message}`,
+        attachments: [
+          {
+            filename: resumeFile.originalname,
+            path: resumeFile.path,
+          },
+        ],
+      };
+
+      try {
+        const info = await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: "Resume sent successfully", info });
+      } catch (error) {
+        console.error("Error sending resume:", error);
+        res
+          .status(500)
+          .json({ error: "Failed to send resume", details: error });
+      }
+    });
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
