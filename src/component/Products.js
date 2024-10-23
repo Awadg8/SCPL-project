4d6e6bd3e14e8aefaf2029996c21d751c6a1bb71
awadg8 <awadheshg798@gmail.com>
2024-10-23 09:27:33 +0530

    Update in Styling Css

--- src/component/Products.js
+++ src/component/Products.js
@@ -673,7 +673,7 @@ function Products() {
       {/* Product area ends here */}
 
       {/* Progress wrap starts here */}
-      <div
+      {/* <div
         id="progress-wrap"
         className="progress-wrap"
         onClick={(e) => {
@@ -688,7 +688,7 @@ function Products() {
         >
           <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM385 215c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-71-71L280 392c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-214.1-71 71c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9L239 103c9.4-9.4 24.6-9.4 33.9 0L385 215z" />
         </svg>
-      </div>
+      </div> */}
       {/* Progress wrap ends here */}
     </div>
   );
